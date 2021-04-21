8dc94558c3806eb3d9d59a49a7d4579f56e29c51
Irina Bainazarova <ira.baynazarova@gmail.com>
2021-04-20 23:47:52 -0700

    added comparator function

--- spec/priority_queue_test.js
+++ spec/priority_queue_test.js
@@ -1,22 +1,52 @@
 describe("PriorityQueue", function() {
     var PriorityQueue = require('../lib/priority_queue');
-
-    beforeEach(function() {
-      priorityQueue = new PriorityQueue();
-    });
   
     it("should return smallest first", function() {
-        priorityQueue.push({value: 5, key: 5});
-        priorityQueue.push({value: 3, key: 3});
-        priorityQueue.push({value: 1, key: 1});
-        priorityQueue.push({value: 2, key: 2});
-        priorityQueue.push({value: 4, key: 4});
+      priorityQueue = new PriorityQueue((a,b) => a.value - b.value);
 
-        expect(priorityQueue.get()).toEqual({value: 1, key: 1});
-        expect(priorityQueue.get()).toEqual({value: 2, key: 2});
-        expect(priorityQueue.get()).toEqual({value: 3, key: 3});
-        expect(priorityQueue.get()).toEqual({value: 4, key: 4});
-        expect(priorityQueue.get()).toEqual({value: 5, key: 5});
-    });
+      priorityQueue.push({value: 5, key: 5});
+      priorityQueue.push({value: 3, key: 3});
+      priorityQueue.push({value: 1, key: 1});
+      priorityQueue.push({value: 2, key: 2});
+      priorityQueue.push({value: 4, key: 4});
+
+      expect(priorityQueue.get()).toEqual({value: 1, key: 1});
+      expect(priorityQueue.get()).toEqual({value: 2, key: 2});
+      expect(priorityQueue.get()).toEqual({value: 3, key: 3});
+      expect(priorityQueue.get()).toEqual({value: 4, key: 4});
+      expect(priorityQueue.get()).toEqual({value: 5, key: 5});
   });
+
+    it("should return biggest first", function() {
+      priorityQueue = new PriorityQueue((a,b) => b.value - a.value);
+
+      priorityQueue.push({value: 5, key: 5});
+      priorityQueue.push({value: 3, key: 3});
+      priorityQueue.push({value: 1, key: 1});
+      priorityQueue.push({value: 2, key: 2});
+      priorityQueue.push({value: 4, key: 4});
+
+      expect(priorityQueue.get()).toEqual({value: 5, key: 5});
+      expect(priorityQueue.get()).toEqual({value: 4, key: 4});
+      expect(priorityQueue.get()).toEqual({value: 3, key: 3});
+      expect(priorityQueue.get()).toEqual({value: 2, key: 2});
+      expect(priorityQueue.get()).toEqual({value: 1, key: 1});
+  });
+
+  it("default comparator", function() {
+    priorityQueue = new PriorityQueue();
+
+    priorityQueue.push(5);
+    priorityQueue.push(4);
+    priorityQueue.push(3);
+    priorityQueue.push(2);
+    priorityQueue.push(1);
+
+    expect(priorityQueue.get()).toEqual(1);
+    expect(priorityQueue.get()).toEqual(2);
+    expect(priorityQueue.get()).toEqual(3);
+    expect(priorityQueue.get()).toEqual(4);
+    expect(priorityQueue.get()).toEqual(5);
+});
+});
   
